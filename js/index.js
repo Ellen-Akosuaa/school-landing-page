ab3d23762359bda7209a6047150cdbaf7400010a
Ellen Ntiamoah <ellenntiamoah@gmail.com>
2024-06-10 09:44:39 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+
+
+const button = document.getElementById('head-button')
+button.onclick = function () { 
+alert('Start Your Childs Educational Journey Here!')
+}
+
+const btn = document.getElementById('head-button2')
+btn.onclick = function () { 
+alert('Open Day Coming Soon!')
+}
+
+const bt = document.getElementById('Read')
+bt.onclick = function () { 
+alert('More About HSS')
+}
+
+const bttn = document.getElementById('students-btn')
+bttn.onclick = function () { 
+alert('The Fun Starts here!')
+}
